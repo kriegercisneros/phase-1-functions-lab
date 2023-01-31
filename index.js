8f4f475e17f7fa86554ee617e044a80dd5959711
JackieCisneros <krieger.jacqueline@gmail.com>
2023-01-30 20:28:46 -0700

    functions, calculating distances with callbacks

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block){
+    if(block>=43){
+        return block-42;
+        }
+    else {
+        return block-26;   
+    } 
+};
+
+function distanceFromHqInFeet(feet){
+    let result = distanceFromHqInBlocks(feet);
+    return result * 264; 
+};
+
+function distanceTravelledInFeet(start, destination){
+  let start1= start * 264;
+  let destination1 = destination * 264;
+  if(start1<destination1){
+    return destination1 - start1}
+  else {
+    return start1 - destination1;
+  }
+};
+function calculatesFarePrice(start, destination){
+    let distance= distanceTravelledInFeet(start, destination);
+    let price = 0;
+    if (distance >= 400 && distance <= 2000) {
+        price = (distance-400)*.02
+    }
+    else if (distance >= 2001 && distance <= 2500) {
+        price = 25;
+    }
+    else if(distance >= 2501) {
+        return "cannot travel that far"
+    }
+    return price;
+};
